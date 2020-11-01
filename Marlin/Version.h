f944b02ce2ba75ab5881b2331c013a24aa4c8a66
kamikazeBDM <35385974+kamikazeBDM@users.noreply.github.com>
2020-11-01 08:18:34 -0800

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -25,7 +25,7 @@
  * Marlin bugfix-2.0.x
  * Compiled version identifier
  ************************************/
-#define COMPILE_VERSION "21" 
+#define COMPILE_VERSION "22" 
 
 #define SHORT_BUILD_VERSION "bugfix-2.0.7.2-" COMPILE_VERSION
 #define STRING_CONFIG_H_AUTHOR "(JAM)" // Who made the changes.
@@ -34,9 +34,14 @@
 
 /*******************************************************************************************************
  * Vx  mm/dd/YY HH:MM 24HR
+
+ * V22 12/xx/20 xx:xx
  * 
- * V21 11/xx/20 xx:xx
- * 
+ * V21 11/31/20 10:07
+ *      upstream fixes
+ * 		!! FAST_PWM support added for STM32F1  !!
+ *		#define FAST_PWM_FAN
+ *	
  * V20 10/28/20 17:28
  *      upstream fixes
  * 
